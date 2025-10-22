2cf35cb255d201f3551f0e1943c2fd5c38fa3363
yangshixian <yangshixian@wujie.14>
2025-10-22 18:17:05 +0800

    在 Vercel 上运行，不会再出现 __dirname is not defined 错误或运行时警告

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,12 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 
-export default NextAuth(authConfig).auth;
+const { auth } = NextAuth(authConfig);
+
+export default auth;
  
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  runtime: 'experimental-edge',
 };
